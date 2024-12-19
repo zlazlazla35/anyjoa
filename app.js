87f51709504e478d0e0268641d1a60f68a74d61e
kimyesol <ww_kw@naver.com>
2024-12-19 18:08:51 +0900

    final completion

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 3001);
 
 app.use(express.static(path.join(__dirname, "/build")));
 
